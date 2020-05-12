e92213db53121da2ccd71dc21e868700e28cb0b5
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-05-12 19:35:27 +0200

    finished the console application to add the data to the DB

--- mqtt-db-logger/src/ALoRa.ConsoleApp/Program.cs
+++ mqtt-db-logger/src/ALoRa.ConsoleApp/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Runtime.InteropServices;
 
 namespace ALoRa.ConsoleApp
 {
@@ -11,25 +13,42 @@ namespace ALoRa.ConsoleApp
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("\nALoRa ConsoleApp - A The Things Network C# Library\n");
 
             var app = new TTNApplication("20180102", "ttn-account-v2.PAUZXYPrQB7VVhB3x_55OsfZrdAQX5S42nxExNSYk_E", "eu");
             app.MessageReceived += App_MessageReceived;
 
-            Console.WriteLine("Press return to exit! HI HI HI HI");
-            Console.ReadLine();
-
-            app.Dispose();
-
-            Console.WriteLine("\nAloha, Goodbye, Vaarwel!");
 
             System.Threading.Thread.Sleep(1000);
         }
 
         private static void App_MessageReceived(TTNMessage obj)
         {
+            AddToDb(obj);
             var data = obj.Payload != null ? BitConverter.ToString(obj.Payload) : string.Empty;
             Console.WriteLine($"Message Timestamp: {obj.Timestamp}, Device: {obj.DeviceID}, Topic: {obj.Topic}, Payload: {data}");
+        }
+
+        private static void AddToDb(TTNMessage obj)
+        {
+            var data = obj.Payload != null ? BitConverter.ToString(obj.Payload) : string.Empty;
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=lora;";
+            string query = "INSERT INTO data(`id`, `timestamp`, `device_id`, `topic`, `payload`) VALUES (NULL, '" + obj.Timestamp + "', '" + obj.DeviceID + "', '" + obj.Topic + "', '" + data + "')";
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+                Console.WriteLine("data succesfully added to DB");
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
